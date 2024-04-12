b033c57e0777d3305b41217db142f932db47d348
Katy DeCorah <katydecorah@gmail.com>
2024-04-11 20:45:22 -0400

    Tags: dingbat, craft; Add swaps

--- test/main-app.test.ts
+++ test/main-app.test.ts
@@ -199,11 +199,11 @@ describe("MainApp", () => {
     const searchStatus = document.querySelector("search-status");
     expect(searchStatus).toMatchInlineSnapshot(`
       <search-status
-        results-length="388"
+        results-length="383"
         selected-tag="need tags"
       >
         <div>
-          Found 388 fonts: 
+          Found 383 fonts: 
         </div>
         
 
